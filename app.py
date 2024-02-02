492eaf5bf9b3a2afe77037c33f2fb3c342217c17
687xyz <140573713+687xyz@users.noreply.github.com>
2024-02-02 10:33:25 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, redirect, url_for
+import json
+
+app = Flask(__name__)
+
+# 加载链接
+def load_links():
+    with open('links.json') as f:
+        return json.load(f)
+
+@app.route('/')
+def home():
+    return '首页'
+
+@app.route('/<name>')
+def goto_link(name):
+    links = load_links()
+    if name in links:
+        return redirect(links[name])
+    else:
+        return '链接不存在!', 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
